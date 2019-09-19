b7c9e7bdf943b13b0cbaded692d49c0a0f70e1b4
new5558 <new17353@gmail.com>
2019-09-20 01:22:52 +0700

    fix layout of form's error text

--- src/app/core/components/register-form/register-form.component.ts
+++ src/app/core/components/register-form/register-form.component.ts
@@ -11,8 +11,9 @@ import { FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterFormComponent implements OnInit {
   @Input('questions') blank = [];
-  @Input() step: Step = {title: null, subtitle: null};
-  questions: BaseQuestion<any>[]  = [
+  @Input() step: Step = { title: null, subtitle: null };
+  precessedQuestions: BaseQuestion<any>[][];
+  questions: BaseQuestion<any>[] = [
     new TextboxQuestion({
       description: 'something 1',
       key: 'question1',
@@ -30,14 +31,49 @@ export class RegisterFormComponent implements OnInit {
       // required: false,
       title: 'QUESTION 2',
       value: 'prefilled value 2'
+    }),
+    new TextboxQuestion({
+      description: 'something 3',
+      key: 'question3',
+      label: 'label 3',
+      order: 3,
+      // required: false,
+      title: 'QUESTION 3',
+      value: 'prefilled value 3'
+    }),
+    new TextboxQuestion({
+      description: 'something 4',
+      key: 'question4',
+      label: 'label 4',
+      order: 4,
+      // required: false,
+      title: 'QUESTION 4',
+      value: 'prefilled value 4'
+    }),
+    new TextboxQuestion({
+      description: 'something 5',
+      key: 'question5',
+      label: 'label 5',
+      order: 5,
+      // required: false,
+      title: 'QUESTION 5',
+      value: 'prefilled value 5'
     })
-  ]
+  ];
+  // this.questions default must be []
   form: FormGroup;
-  constructor(private formGenerator: FormGeneratorService) {
-  }
-  
+  constructor(private formGenerator: FormGeneratorService) {}
+
   ngOnInit() {
     this.form = this.formGenerator.toFormGroup(this.questions);
+    this.precessedQuestions = this.questions
+      .sort((a, b) => a.order - b.order)
+      .reduce((result, question, i) => {
+        i % 2 === 0
+          ? result.push([question])
+          : result[result.length - 1].push(question);
+        return result;
+      }, []);
+    console.log(this.precessedQuestions, 'q');
   }
-
 }
