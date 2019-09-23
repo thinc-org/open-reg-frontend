c3ccd077b6a48f2de051fa314633acea5805f994
Non Eks <ratchanoneks@gmail.com>
2019-09-23 17:27:39 +0700

    refactor: add theme object

--- src/app/core/components/topic/topic.component.ts
+++ src/app/core/components/topic/topic.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+export interface Theme {
+  mainTextColor: string;
+  subTextColor: string;
+}
+
 @Component({
   selector: 'topic',
   templateUrl: './topic.component.html',
   styleUrls: ['./topic.component.scss']
 })
+
 export class TopicComponent implements OnInit {
   @Input() mainText = null;
-  @Input() mainTextColor = '#000000';
-  @Input() textSpace = '5px'
-  @Input() mainTextSize = '20px'
+  @Input() textSpace = '5px';
+  @Input() mainTextSize = '20px';
   @Input() subText = null;
-  @Input() subTextColor = '#B3B3B3';
   @Input() subTextSize = '10px';
   @Input() subTextLs = 'normal';
   @Input() textPos = 'left';
-  
+  @Input() theme: Theme = { mainTextColor: '#000000', subTextColor: '#B3B3B3' };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
