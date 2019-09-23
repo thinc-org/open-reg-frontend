66b192b7f52dae0f935c6db25b8d16e5b4a7313d
Non Eks <ratchanoneks@gmail.com>
2019-09-23 17:28:01 +0700

    refactor: remove console.log

--- src/app/core/directives/dimension.directive.ts
+++ src/app/core/directives/dimension.directive.ts
@@ -5,7 +5,6 @@ import { Directive, Input, ElementRef, AfterViewInit } from '@angular/core';
 })
 export class DimensionDirective implements AfterViewInit{
   ngAfterViewInit(){
-    console.log(this.el, 'newnew');
     let style = this.el.nativeElement.children[0].style;
     style.height = `${this.height}px`;
     style.width = `${this.width}px`;
