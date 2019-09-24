80ca9e7cd54ae8b618cefd6edc68b36bbb042fc4
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2019-09-24 15:16:23 +0700

    :bug: :ambulance: import core module

--- src/app/modules/attend/attend.module.ts
+++ src/app/modules/attend/attend.module.ts
@@ -5,11 +5,13 @@ import { AttendRoutingModule } from './attend-routing.module';
 import { AttendComponent } from './attend.component';
 import { NzListModule, NzResultModule, NzButtonModule } from 'ng-zorro-antd';
 import { AttendSuccessComponent } from './attend-success/attend-success.component';
+import { CoreModule } from 'src/app/core/core.module';
 
 @NgModule({
   declarations: [AttendComponent, AttendSuccessComponent],
   imports: [
     CommonModule,
+    CoreModule,
     AttendRoutingModule,
     NzListModule,
     NzResultModule,
