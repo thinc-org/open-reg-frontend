944464c83b1ae641db323c8c9c05d3bf365b6702
new5558 <new17353@gmail.com>
2019-09-24 21:01:58 +0700

    refactor: remove processedQuestions subscription logic and use async pipe instead

--- src/app/core/components/register-form/register-form.component.ts
+++ src/app/core/components/register-form/register-form.component.ts
@@ -1,44 +1,26 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BaseQuestion } from '../../model/questions.model';
 import { FormGroup } from '@angular/forms';
 import { RegisterService } from 'src/app/modules/register/register.service';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { map, takeUntil } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.scss'],
 })
-export class RegisterFormComponent implements OnInit, OnDestroy {
+export class RegisterFormComponent {
   @Input() form: FormGroup;
+  questions$: BehaviorSubject<BaseQuestion<any>[]> = this.registerService
+    .questions$;
+  eventName: string = this.registerService.eventName;
+  processedQuestions$: Observable<BaseQuestion<any>[][]> = this.questions$.pipe(
+    map(e => (e ? e.sort((a, b) => a.order - b.order) : [])),
+    map(this.reduce)
+  );
 
-  questions$: BehaviorSubject<BaseQuestion<any>[]>;
-  eventName: string;
-  precessedQuestions$: Observable<BaseQuestion<any>[][]>;
-  processedQuestions: BaseQuestion<any>[][];
-  destroy$ = new Subject<any>();
-
-  constructor(private registerService: RegisterService) {
-    this.questions$ = this.registerService.questions$;
-    this.eventName = this.registerService.eventName;
-  }
-
-  ngOnInit() {
-    this.precessedQuestions$ = this.questions$.pipe(
-      takeUntil(this.destroy$),
-      map(e => (e ? e.sort((a, b) => a.order - b.order) : [])),
-      map(this.reduce)
-    );
-    this.precessedQuestions$.subscribe(result => {
-      this.processedQuestions = result;
-    });
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next(undefined);
-    this.destroy$.unsubscribe();
-  }
+  constructor(private registerService: RegisterService) {}
 
   private reduce(array: BaseQuestion<any>[]) {
     return array.reduce((result, question, i) => {
