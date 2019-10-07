95907a7637ab8755e82dc6092fc76e688224d4fc
new5558 <new17353@gmail.com>
2019-10-07 20:44:28 +0700

    feat: add link to home in openreg logo

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router, private authService: AuthService) {}
   ngOnInit() {}
+
+  login() {
+    if (this.authService.isAuthenticated) {
+      this.router.navigate(['profile']);
+    } else {
+      this.router.navigate(['login']);
+    }
+  }
 }
