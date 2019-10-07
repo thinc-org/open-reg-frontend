2ff3a13ad3ff27f337e3611128178b1171780413
new5558 <new17353@gmail.com>
2019-10-07 20:30:45 +0700

    fix, refactor: merge old navbar code with new one

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  currentUser$ = this.authService.currentUser$;
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {}
 }
