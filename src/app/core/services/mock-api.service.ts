dc61c79194adb6d5f0988eb18930f2bfe533151f
new5558 <new17353@gmail.com>
2019-11-02 00:51:13 +0700

    test: add mock api service for user form

--- src/app/core/services/mock-api.service.ts
+++ src/app/core/services/mock-api.service.ts
@@ -1,81 +1,175 @@
 import { Injectable } from '@angular/core';
-import { ApiInterface } from './api.service';
 import { Observable, timer, Subject } from 'rxjs';
-import { TextboxQuestion, DropdownQuestion } from '../model/questions.model';
-import { Validators } from '@angular/forms';
+import { ApiService } from 'src/app/api/services';
+import { HttpClient } from '@angular/common/http';
+import { ApiConfiguration as __Configuration } from '../../api/api-configuration';
 
 @Injectable({
   providedIn: 'root',
 })
-export class MockApiService implements ApiInterface {
-  get<T>(
-    url: string,
-    params?: { [param: string]: string | string[] }
-  ): Observable<T> {
-    switch (url) {
-      case 'questions':
-        const response = new Subject<any>();
-        timer(1000).subscribe(() => {
-          response.next({
-            questions: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((_, i) => {
-              i += 1;
-              if (i === 4) {
-                return new DropdownQuestion(
-                  {
-                    description: `something special`,
-                    key: `question dropdown`,
-                    label: `label dropdown`,
-                    order: 30,
-                    validators: [Validators.required], // not dynamic yet
-                    title: 'QUESTION special',
-                    // value: null,
-                    group: 1,
-                    choices: ['go', 'go2', 'go3'],
-                  },
-                  'dropdown'
-                );
-              } else if (i === 5) {
-                return new DropdownQuestion(
-                  {
-                    description: `something special`,
-                    key: `question dropdown 2`,
-                    label: `label dropdown`,
-                    order: 30,
-                    validators: [Validators.required], // not dynamic yet
-                    title: 'QUESTION special',
-                    value: '0',
-                    group: 1,
-                    choices: ['go', 'go2', 'go3'],
-                  },
-                  'radio'
-                );
-              } else {
-                return new TextboxQuestion({
-                  description: `something ${i}`,
-                  key: `question ${i}`,
-                  label: `label ${i}`,
-                  order: i,
-                  validators: [Validators.required, Validators.email], // not dynamic yet
-                  title: `QUESTION ${i}`,
-                  value: `prefilled value`,
-                  type: 'email',
-                  group: Math.ceil(i / 4),
-                });
-              }
-            }),
-            group: [
-              { n: 1, title: 'Group A', description: 'ABC' },
-              { n: 2, title: 'Group B', description: 'ABC' },
-              { n: 3, title: 'Group C', description: 'ABC' },
-            ],
-            title: 'F: Practical Concrete Pants?',
-          });
-        });
-        return response;
-      default:
-        console.log(params);
-    }
-    throw new Error('Method not implemented.');
+export class MockApiService extends ApiService {
+  getUserForm(): Observable<any> {
+    const response = new Subject<any>();
+    response.subscribe((e) => console.log(e, 'e'));
+    response.next({});
+    timer(1000).subscribe(() => {
+      response.next({
+        eventId: '',
+        title: 'ผู้ใช้',
+        groups: [
+          {
+            title: 'ข้อมูลส่วนตัว',
+            description: ' Personal Information',
+            order: 1,
+          },
+          // {
+          //   title: 'ข้อมูลการติดต่อ',
+          //   description: 'Contact Information',
+          //   order: 2,
+          // },
+        ],
+        questions: [
+          {
+            key: 'file',
+            value: '',
+            order: 1,
+            group: 1,
+            type: 'FILE',
+            label: 'รูปภาพ',
+            required: true,
+          },
+          // {
+          //   key: 'title',
+          //   value: '0',
+          //   order: 1,
+          //   group: 1,
+          //   type: 'DROPDOWN',
+          //   label: 'คำนำหน้า (ภาษาไทย)',
+          //   choices: ['นาย', 'นางสาว', 'นาง'],
+          //   required: true,
+          // },
+          // {
+          //   key: 'firstName',
+          //   value: 'นรภัทร',
+          //   order: 2,
+          //   group: 1,
+          //   type: 'TEXT',
+          //   label: 'ชื่อ (ภาษาไทย)',
+          //   required: true,
+          // },
+          // {
+          //   key: 'lastName',
+          //   value: 'บุพโพดม',
+          //   order: 3,
+          //   group: 1,
+          //   type: 'TEXT',
+          //   label: 'นามสกุล (ภาษาไทย)',
+          //   required: true,
+          // },
+          // {
+          //   key: 'nickName',
+          //   value: 'นิว',
+          //   order: 4,
+          //   group: 1,
+          //   type: 'TEXT',
+          //   label: 'ชื่อเล่น (ภาษาไทย)',
+          //   required: true,
+          // },
+          // {
+          //   key: 'titleEn',
+          //   value: '0',
+          //   order: 5,
+          //   group: 1,
+          //   type: 'DROPDOWN',
+          //   label: 'คำนำหน้า (ภาษาอังกฤษ)',
+          //   choices: ['Mr.', 'Ms.', 'Mrs.'],
+          //   required: true,
+          // },
+          // {
+          //   key: 'firstNameEn',
+          //   value: 'Norapat',
+          //   order: 6,
+          //   group: 1,
+          //   type: 'TEXT',
+          //   label: 'ชื่อ (ภาษาอังกฤษ)',
+          //   required: true,
+          // },
+          // {
+          //   key: 'lastNameEn',
+          //   value: 'Buppodom',
+          //   order: 7,
+          //   group: 1,
+          //   type: 'TEXT',
+          //   label: 'นามสกุล (ภาษาอังกฤษ)',
+          //   required: true,
+          // },
+          // {
+          //   key: 'nickNameEn',
+          //   value: 'New',
+          //   order: 8,
+          //   group: 1,
+          //   type: 'TEXT',
+          //   label: 'ชื่อเล่น (ภาษาอังกฤษ)',
+          //   required: true,
+          // },
+          // {
+          //   key: 'tel',
+          //   value: '0863435490',
+          //   order: 1,
+          //   group: 2,
+          //   type: 'PHONE',
+          //   label: 'เบอร์โทรศัพท์',
+          //   required: true,
+          // },
+          // {
+          //   key: 'email',
+          //   value: 'new17353@gmail.com',
+          //   order: 2,
+          //   group: 2,
+          //   type: 'EMAIL',
+          //   label: 'อีเมล',
+          //   required: true,
+          // },
+          // {
+          //   key: 'emergencyTel',
+          //   value: '0898163862',
+          //   order: 3,
+          //   group: 2,
+          //   type: 'PHONE',
+          //   label: 'เบอร์โทรศัพท์ฉุกเฉิน',
+          //   required: true,
+          // },
+          // {
+          //   key: 'emergencyRelationship',
+          //   value: 'แม่',
+          //   order: 4,
+          //   group: 2,
+          //   type: 'TEXT',
+          //   label: 'มีความสัมพันธ์เป็น',
+          //   required: true,
+          // },
+          // {
+          //   key: 'line',
+          //   value: '+7353',
+          //   order: 5,
+          //   group: 2,
+          //   type: 'TEXT',
+          //   label: 'LINE ID',
+          //   required: true,
+          // },
+          // {
+          //   key: 'facebook',
+          //   value: 'new norapat',
+          //   order: 6,
+          //   group: 2,
+          //   type: 'TEXT',
+          //   label: 'Facebook',
+          //   required: true,
+          // },
+        ],
+      });
+    });
+    return response;
   }
   post<T>(
     url: string,
@@ -99,5 +193,7 @@ export class MockApiService implements ApiInterface {
     throw new Error('Method not implemented.');
   }
 
-  constructor() {}
+  constructor(config: __Configuration, http: HttpClient) {
+    super(config, http);
+  }
 }
