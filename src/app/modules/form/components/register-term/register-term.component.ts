bab61fc9ee9ffd19cf27f8939155f0e0d4b8f187
new5558 <new17353@gmail.com>
2019-11-06 22:26:43 +0700

    fix: navbar cu-tu ball missing year

---
+++ src/app/modules/form/components/register-term/register-term.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-term',
+  templateUrl: './register-term.component.html',
+  styleUrls: ['./register-term.component.scss']
+})
+export class RegisterTermComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
