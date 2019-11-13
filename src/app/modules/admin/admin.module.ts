c4746cbdda05f06377278ec072c1a149d29da21f
new5558 <new17353@gmail.com>
2019-11-13 16:31:41 +0700

    refactor(admin): remove ant design

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { NzButtonModule, NzTypographyModule } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [AdminComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    NzButtonModule,
-    NzTypographyModule,
   ],
 })
 export class AdminModule {}
