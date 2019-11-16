2128baf949a75cdd556ea44c6c94b60cfbfc3016
new5558 <new17353@gmail.com>
2019-11-16 21:32:28 +0700

    feat: redirect user back to their pervious location after login success

--- src/app/core/guards/admin-guard.service.ts
+++ src/app/core/guards/admin-guard.service.ts
@@ -2,18 +2,26 @@ import { Injectable } from '@angular/core';
 import { Router, CanActivateChild } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { withLatestFrom, map } from 'rxjs/operators';
+import { Location } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminGuard implements CanActivateChild {
-  constructor(public auth: AuthService, public router: Router) {}
+  constructor(
+    public auth: AuthService,
+    public router: Router,
+    private location: Location
+  ) {}
   canActivateChild() {
     return this.auth.checkExportable().pipe(
       withLatestFrom(this.auth.currentUser$),
       map(([ids, user]) => {
         if (!ids.includes(user.info.chulaId)) {
-          this.router.navigate(['/']);
+          const currentURL = this.location.path();
+          this.router.navigate(['/'], {
+            queryParams: { return: currentURL },
+          });
           return false;
         }
         return true;
