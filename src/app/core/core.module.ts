bcef31d46764511eec1dbb192d9b4f0da6c0e447
new5558 <new17353@gmail.com>
2020-06-13 18:24:08 +0700

    :sparkles: add custom component example

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ExampleComponent } from './components/example/example.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, ExampleComponent],
   imports: [],
   providers: [],
   bootstrap: [],
+  exports: [NavbarComponent, ExampleComponent],
 })
 export class CoreModules {}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 
+import { CoreModules } from 'src/app/core/core.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [HomeComponent, LogoComponent],
-  imports: [CommonModule, HomeRoutingModule, MatCardModule],
+  imports: [CommonModule, HomeRoutingModule, CoreModules, MatCardModule],
 })
 export class HomeModule {}
