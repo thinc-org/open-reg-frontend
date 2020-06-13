013bfad5346202d97a95085cc49c0bc5ec34e1c1
Norapat Buppodom <new17353@gmail.com>
2020-06-13 14:40:14 +0700

    feat/reconfig project settings (#54)
    * :wrench: gitignore .now folder
    
    * :wrench::heavy_plus_sign: using lint-staged and ng-lint-staged
    
    * :wrench::heavy_plus_sign: use prettier-quick to format staged files instead of prettier --write
    
    * :wrench::bug: fix lintstaged config
    
    * :wrench: add vscode config
    
    * :zap::heavy_plus_sign: migrate to eslint
    
    * :fire: remove tslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  extends: ['plugin:@angular-eslint/recommended'],
+  rules: {
+    '@angular-eslint/directive-selector': [
+      'error',
+      { type: 'attribute', prefix: 'app', style: 'camelCase' },
+    ],
+    '@angular-eslint/component-selector': [
+      'error',
+      { type: 'element', prefix: 'app', style: 'kebab-case' },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.component.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      plugins: ['@angular-eslint/template'],
+      processor: '@angular-eslint/template/extract-inline-html',
+    },
+    {
+      files: ['src/**/*.spec.ts', 'src/**/*.d.ts'],
+      parserOptions: {
+        project: './src/tsconfig.spec.json',
+      },
+      extends: ['plugin:jasmine/recommended'],
+      plugins: ['jasmine'],
+      env: { jasmine: true },
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+    {
+      files: ['*.ts'],
+      extends: [
+        'airbnb-typescript/base',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      rules: {
+        'class-methods-use-this': 'off',
+        'import/prefer-default-export': 'off',
+        'lines-between-class-members': 'off',
+        'no-useless-constructor': 'off',
+        'no-empty-function': 'off',
+        '@typescript-eslint/no-useless-constructor': 0,
+        '@typescript-eslint/no-empty-function': 0,
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      },
+    },
+  ],
+};
