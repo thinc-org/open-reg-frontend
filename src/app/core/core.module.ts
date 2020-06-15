c6a81e4be55727336cc18abb7e970f8a933d6202
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-06-15 17:38:43 +0700

    create: full logo component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ExampleComponent } from './components/example/example.component';
 import { ThemeControllerComponent } from './components/theme-controller/theme-controller.component';
+import { FullLogoComponent } from './components/full-logo/full-logo.component';
 
 @NgModule({
-  declarations: [NavbarComponent, ExampleComponent, ThemeControllerComponent],
-  imports: [],
+  declarations: [NavbarComponent, ExampleComponent, ThemeControllerComponent, FullLogoComponent],
+  imports: [MatToolbarModule],
   providers: [],
   bootstrap: [],
   exports: [NavbarComponent, ExampleComponent, ThemeControllerComponent],
