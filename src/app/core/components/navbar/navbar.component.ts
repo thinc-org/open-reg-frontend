61974909f2682d984057af6c1e27e894479c5196
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-06-17 21:58:55 +0700

    ongoing: implement navbar

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  menus: any[] = [
+    {
+      title: 'Getting Started',
+      link: '/',
+    },
+    {
+      title: 'Getting Started 2',
+      link: '/',
+    },
+    {
+      title: 'Getting Started 3',
+      link: '/',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit() {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ExampleComponent } from './components/example/example.component';
 import { ThemeControllerComponent } from './components/theme-controller/theme-controller.component';
@@ -7,7 +12,14 @@ import { FullLogoComponent } from './components/full-logo/full-logo.component';
 
 @NgModule({
   declarations: [NavbarComponent, ExampleComponent, ThemeControllerComponent, FullLogoComponent],
-  imports: [MatToolbarModule],
+  imports: [
+    MatToolbarModule,
+    MatDividerModule,
+    MatListModule,
+    MatIconModule,
+    CommonModule,
+    RouterModule,
+  ],
   providers: [],
   bootstrap: [],
   exports: [NavbarComponent, ExampleComponent, ThemeControllerComponent],
