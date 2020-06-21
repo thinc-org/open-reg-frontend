121a494a18273c5628cfd92ea3ed3272cc471385
new5558 <new17353@gmail.com>
2020-06-21 13:52:23 +0700

    :green-check-marks: fix failed test 2

--- src/app/core/components/pagination/pagination.component.ts
+++ src/app/core/components/pagination/pagination.component.ts
@@ -97,7 +97,10 @@ export class PaginationComponent implements AfterViewInit {
 
   getCompleteWidth(el?: ElementRef<any>) {
     const element = el?.nativeElement;
-    const style = element?.currentStyle || window.getComputedStyle(element);
+    if (element === undefined) {
+      return 0;
+    }
+    const style = element.currentStyle || window.getComputedStyle(element);
     const { offsetWidth } = element;
     const margin = parseFloat(style.marginLeft) + parseFloat(style.marginRight);
     return offsetWidth + margin;
@@ -105,7 +108,10 @@ export class PaginationComponent implements AfterViewInit {
 
   getPadding(el?: ElementRef<any>) {
     const element = el?.nativeElement;
-    const style = element?.currentStyle || window.getComputedStyle(element);
+    if (element === undefined) {
+      return 0;
+    }
+    const style = element.currentStyle || window.getComputedStyle(element);
     const padding = parseFloat(style.paddingLeft) + parseFloat(style.paddingRight);
     return padding;
   }
