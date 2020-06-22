0ef134eb1461d84ec5fe78b008c838e14b596e95
new5558 <new17353@gmail.com>
2020-06-23 02:54:13 +0700

    :bug: ci build error

--- src/app/core/components/event-card/event-card.component.ts
+++ src/app/core/components/event-card/event-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Event } from 'src/backend-client/model/models';
+import { Event } from 'backend-client/model/models';
 import { EventCardType } from '../../types/constant';
 
 @Component({
