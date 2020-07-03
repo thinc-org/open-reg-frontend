5b9b8aa1f8930c45cc59eebefb8e59ece8a0c5c7
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-07-03 21:57:31 +0700

    update: onSubmit method
    Signed-off-by: Saenyakorn Siangsanoh <saneyakorn@gmail.com>

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Location } from '@angular/common';
 import { AuthenticateService } from '../../core/services/authenticate.service';
+import { ApiService } from '../../../backend-client';
 
 @Component({
   selector: 'app-login',
@@ -11,26 +11,43 @@ import { AuthenticateService } from '../../core/services/authenticate.service';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  redirectUrl = '/';
 
   constructor(
     private authService: AuthenticateService,
     private formBuilder: FormBuilder,
-    private location: Location,
+    private apiService: ApiService,
     private router: Router
   ) {
     this.loginForm = formBuilder.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
     });
+
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation && navigation.extras.state) {
+      this.redirectUrl = navigation.extras.state.redirectUrl;
+    }
   }
 
   ngOnInit(): void {}
 
   onSubmit(): void {
     if (!this.loginForm.invalid) {
-      console.log('valid');
-      this.authService.login(this.loginForm.value.email, this.loginForm.value.password);
-      this.router.navigate(['/']);
+      const { email } = this.loginForm.value;
+      const { password } = this.loginForm.value;
+      this.apiService.authControllerLogin({ email, password }).subscribe(
+        (authToken) => {
+          this.authService.login(authToken);
+
+          // this should redirect back to all previous route, not only page that have auth guard.
+          this.router.navigate([this.redirectUrl]);
+        },
+        (error) => {
+          // sending error popup or message
+          console.log(error);
+        }
+      );
     }
   }
 }
