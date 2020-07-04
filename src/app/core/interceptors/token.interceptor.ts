07c0a60d30ce29b3f7dec600a56803559e12ff6d
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-07-04 14:06:52 +0700

    update: intercept() method
    Signed-off-by: Saenyakorn Siangsanoh <saneyakorn@gmail.com>

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthenticateService } from '../services/authenticate.service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(public authenticateService: AuthenticateService) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const result = request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${this.authenticateService.getToken()}`,
+      },
+    });
+    return next.handle(result);
   }
 }
