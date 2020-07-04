c382117039387ca82382c84d97180d17f63d196b
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-07-04 13:29:21 +0700

    create: primary token interceptor
    Signed-off-by: Saenyakorn Siangsanoh <saneyakorn@gmail.com>

---
+++ src/app/core/interceptors/token.interceptor.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request);
+  }
+}
